fc2a307e3a64263f63b0b8f70598a85b3eb5a819
Johanna Heidel <92253630+HdlJohanna@users.noreply.github.com>
2022-03-26 22:52:21 +0100

    Fix ~.push

--- webserver.py
+++ webserver.py
@@ -1,26 +1,57 @@
-from flask import Flask, request
-from itsdangerous import json
+from flask import Flask, redirect, render_template, request, url_for
 from werkzeug import Response
 import requests
+from flask_dance.contrib.github import make_github_blueprint,github 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "636c626fbb87e4b10d7b"
+gh_blueprint = make_github_blueprint(
+    client_id="636c626fbb87e4b10d7b",
+    client_secret="91a813e210f2eadbcf3ab19839f3a387ee54454b",
+)
+app.register_blueprint(gh_blueprint,url_prefix="/oauth")
 
 @app.route("/")
 def main():
-  return Response("Success")
+    return render_template("index.html")
+
+@app.route("/source")
+def src():
+    return redirect("https://github.com/HdlJohanna/GitHubBot")
+
+@app.route("/discord")
+def dc():
+    return redirect("https://discord.gg/RVppWSxK3P")
+
+@app.route("/invite")
+def invite():
+    return redirect("Linhttps://discord.com/oauth2/authorize?client_id=899236006769860658&scope=bot&permissions=536963313")
+
+
+@app.route("/oauth/github/authorized")
+def authed():
+    code = request.args.get("code")
+    resp = requests.post("https://github.com/login/oauth/access_token",data={
+        "state":"oauthme",
+        "client_id":"636c626fbb87e4b10d7b",
+        "client_secret":"91a813e210f2eadbcf3ab19839f3a387ee54454b",
+        "redirect_uri":"https://github.zerotwo36.repl.co/oauth/github/authorized",
+        "code":code
+    })
+    return resp.json()
 
 @app.route("/github",methods=["GET", "POST"])
-def github():
+def github_():
     print(request.headers)
     if request.headers.get("X-Github-Event") == "push":
       print(len(request.json['commits']))
+      url = f'https://github.com/{request.json.get("repository")["full_name"]}/commits/{request.json["commits"][-1]["id"]}'
       requests.post(request.args.get("payload"), json={
             'embeds':[
                 {
                     'title':"🔔 New Commit!",
-                    'url':request.json.get('commits_url'),
                     'color':0x6D00FF,
-                    'description':f'**{len(request.json["commits"])}** Commits on {request.json.get("repository")["full_name"]}\n\n**Commit by** {request.json.get("commits")[0]["author"]["username"]}\n\n**Message** {request.json.get("commits")[0]["message"]}\n\n**Commit URL**: {request.json.get("commits_url")}'
+                    'description':f'**{len(request.json["commits"])}** Commits on {request.json.get("repository")["full_name"]}\n\n**Commit by** {request.json.get("commits")[0]["author"]["username"]}\n\n**Message** {request.json.get("commits")[-1]["message"]}\n\n**Commit URL**: {url}'
                     
                 }
             ]
@@ -74,7 +105,7 @@ def github():
                 {
                     'title':f"👥 Collabs updated",
                     'color':0x6D00FF,
-                    'description':f'Collaborator: {request.json.get("member")["login"]} {request.json.get("member")["login"]} on {request.json["repository"]["full_name"]}'
+                    'description':f'Collaborator: {request.json.get("member")["login"]} on {request.json["repository"]["full_name"]}'
                 }
             ]
         })
